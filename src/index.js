098ecc75cc2cbe9e44581e40653dc31d2ac5dbe1
zebramilk <zebramilkx@gmail.com>
2022-10-24 17:59:33 -0500

    Add css-loader and style sheet and webpack asset managhement.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import "./style.css";
+
+
+
 console.log("it works, thus far");
+
+const cards = document.getElementsByClassName("card")
+
+
+cards.forEach(() => {
+    this.classList.push("light")
+});
