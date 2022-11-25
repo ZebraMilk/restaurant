32d40f48bef3f81c5e0496a4750fa1910bb10f4a
zebramilk <zebramilkx@gmail.com>
2022-11-25 14:43:17 -0600

    Establish load.js as a working dependency of index.js.
    Succesfully loads the nav bar.
    Next task is to load some dummy drink and food cards with images
    and make the layout look okay.

--- src/load.js
+++ src/load.js
@@ -1,11 +1,12 @@
 export default loadInitialHeader;
 
-function loadInitialHeader() {
+const content = document.getElementById("content");
 
-    const content = document.getElementById("content");
+function loadInitialHeader() {
     const header = document.createElement("header");
     const navMenu = document.createElement("nav");
     navMenu.id = "nav-menu";
+
     const navList = ["Home", "Menu", "About"];
 
     navList.forEach(title => {
@@ -20,3 +21,8 @@ function loadInitialHeader() {
     header.appendChild(navMenu);
     content.appendChild(header);
 };
+
+// clear the displayed page contents except for the nav bar
+
+// load each page onClick
+
---
+++ src/menu.js
@@ -0,0 +1,25 @@
+export default loadMenu;
+
+const newCard = (card) 
+
+function loadMenu() {
+    // make the menuContainer for drinks and food
+    const menuContainer = document.createElement("main");
+    
+    // make the container for the drink cards
+    const drinksHeading = document.createElement("h2")
+    drinksHeading.classList.add("menu-type");
+    drinksHeading.id = "drinks";
+    drinksHeading.textContent = "Drinks";
+
+    
+
+    // make a drink card
+
+
+    // make the container for the food cards
+
+    // make a food card
+
+    console.log("Menu Loaded");
+}
