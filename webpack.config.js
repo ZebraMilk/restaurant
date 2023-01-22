a7424470ed801f82dcdc6d9d3fd1099551c0cef7
zebramilk <zebramilkx@gmail.com>
2023-01-22 17:07:10 -0600

    Install some loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: "development", //production
@@ -8,5 +9,24 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].bundle.js",
-  }
+    clean: true,
+  },
+  //loaders
+  module: {
+    rules: [
+      // css loader, order is important here
+      {test: /\.css$/i, use: ['style-loader', 'css-loader']}
+    ]
+  },
+
+
+  //plugins
+  plugins: [
+    new htmlWebpackPlugin({
+      title: "The Listing Lich",
+      filename: "index.html",
+      template: path.resolve(__dirname, "src/template.html"),
+
+    })
+  ],
 }
